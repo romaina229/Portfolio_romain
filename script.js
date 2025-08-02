dba5753b41536fda2d515b55f25e4f60da063725
romaina229 <nounonvenanromain@gmail.com>
2025-08-02 02:56:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function afficherContact() {
+  var email = document.getElementById("email");
+  if (email.style.display === "none") {
+    email.style.display = "block";
+  } else {
+    email.style.display = "none";
+  }
+}
