9f260af588bc504536889f8bd699573a88b22714
AKPO <nounonvenanromain@gmail.com>
2025-08-04 18:38:29 +0100

    Add files via upload
    Signed-off-by: AKPO  <nounonvenanromain@gmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+// Gérer l'envoi du formulaire
 document.getElementById("contact-form").addEventListener("submit", function(e) {
   e.preventDefault();
   alert("Message envoyé (simulation). Merci de votre contact !");
 });
+
+// Gérer l'affichage des sections
+function showSection(id) {
+  const sections = document.querySelectorAll("section");
+  sections.forEach(section => section.classList.add("hidden"));
+
+  const selected = document.getElementById(id);
+  if (selected) {
+    selected.classList.remove("hidden");
+  }
+}
